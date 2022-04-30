ae2c226b922f442bc32ef2e42d8e3b75422fc404
Alex Yozhkin- <>
2022-04-30 14:30:50 +0300

    Create UIButton extansion for touch animation

---
+++ WordleGame/WordleGame/Extensions/UIButton + Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIButton + Extension.swift
+//  WordleGame
+//
+//  Created by Alex on 30.04.22.
+//
+
+import UIKit
+
+extension UIButton {
+    override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+       {
+        self.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 6, options: .allowUserInteraction, animations: {
+            self.transform = CGAffineTransform.identity
+        }, completion: nil)
+        super.touchesBegan(touches, with: event)
+      }
+}
