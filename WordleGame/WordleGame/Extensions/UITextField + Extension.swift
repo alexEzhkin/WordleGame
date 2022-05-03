dd96b69d57fbd8dfa55c13b3ec9a1e16e63a1b47
Alex Yozhkin- <>
2022-05-04 01:01:56 +0300

    Added extension to UITextField for maxlength property

---
+++ WordleGame/WordleGame/Extensions/UITextField + Extension.swift
@@ -0,0 +1,31 @@
+//
+//  UITextField + Extension.swift
+//  WordleGame
+//
+//  Created by Alex on 4.05.22.
+//
+
+import Foundation
+import UIKit
+
+private var __maxLengths = [UITextField: Int]()
+
+extension UITextField {
+    @IBInspectable var maxLength: Int {
+        get {
+            guard let l = __maxLengths[self] else {
+                return 150 // (global default-limit. or just, Int.max)
+            }
+            return l
+        }
+        set {
+            __maxLengths[self] = newValue
+            addTarget(self, action: #selector(fix), for: .editingChanged)
+        }
+    }
+    @objc func fix(textField: UITextField) {
+        if let t = textField.text {
+            textField.text = String(t.prefix(maxLength))
+        }
+    }
+}
